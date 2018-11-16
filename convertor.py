f6c6ec0c44998d70cef0a9bed350ef7da8ab1dee
yceugenelai <yceugenelai@gmail.com>
2018-11-16 14:59:24 +0800

    fix some bugs

--- convertor.py
+++ convertor.py
@@ -4,9 +4,9 @@ import numpy as np
 from skimage.transform import resize
 import os
 
-def simple_image_concat(filename , frame_num, img_list, concat_axis):
+def simple_image_concat(filename , save_name, img_list, concat_axis):
 	i_new = np.concatenate(tuple(img_list), axis=concat_axis)
-	imageio.imwrite(filename + "/" + str(frame_num) + ".jpg", i_new[:, :, :])
+	imageio.imwrite(filename + "/" + str(save_name) + ".jpg", i_new[:, :, :])
 
 
 def video_to_dataset(filename, concat_axis, concat_size, frame_range = [0,-1] , frame_clip=[0,0,-1,-1]
@@ -19,9 +19,10 @@ def video_to_dataset(filename, concat_axis, concat_size, frame_range = [0,-1] , 
 	if not os.path.exists("./" + filename):
 		os.makedirs(filename)	
 
+	save_file_count = 1
 	for i, im in enumerate(reader):
 		if i % 1000 == 0:
-			print(i)
+			print(i, im.shape)
 		if i >= frame_range[0]:
 			if len(img_list) >= concat_size:
 				img_list.remove(img_list[0])
@@ -36,8 +37,9 @@ def video_to_dataset(filename, concat_axis, concat_size, frame_range = [0,-1] , 
 			
 			img_list.append(tmp_im)
 			if len(img_list) == concat_size:
-				simple_image_concat(filename, i, img_list,concat_axis)
-		if i != -1 and i >= frame_range[1]:
+				simple_image_concat(filename, save_file_count, img_list,concat_axis)
+				save_file_count = save_file_count + 1
+		if frame_range[1] != -1 and i >= frame_range[1]:
 			break
 
 if __name__ == '__main__':
